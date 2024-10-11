7334700c4a7e0830b2f4c8a470bb3d0c143743eb
MoSheckon <mch239@nau.edu>
2024-10-10 17:44:38 -0700

    Work on 'Cannot POST /home' error

--- index.js
+++ index.js
@@ -16,64 +16,58 @@ const db = new pg.Client({
 
 db.connect();
 
-//middleware
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
 
-//Following Permalist Project's lead on putting default Blog titles and number. 
-let blogsList = [
-    { id: 1, title: "October Scare" },
-    { id: 2, title: "Ready for Halloween" },
-];
+let blogs = [];
 
 app.get("/", async (req, res) => {
-    try {
-        const result = await db.query("SELECT * from blogs ORDER BY id ASC");
-        blogs = result.rows;
-        
-        res.render("index.ejs", {
-            blogsTitle: "Today",
-            blogsList: blogs, 
-        });
-    }   catch (err) {
-            console.log(err);
-    }
+  try {
+    const result = await db.query("SELECT * FROM blogs ORDER BY id ASC");
+    blogs = result.rows;
+
+    res.render("index.ejs", {
+      listTitle: "Today",
+      listBlogs: blogs,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-app.post("/home", (req, res) => {
-    const blogsTitle = req.body.blogsTitle;
-    const blogsText = req.body.blogsText;
-    blogsList.push({
-        id: generateID(),
-        title: blogsTitle,
-        description: blogsText,
-    });
-    res.render(homePath, {
-        blogsList: blogsList,
-    });
+app.post("/add", async (req, res) => {
+  const item = req.body.newItem;
+  // blogs.push({title: item});
+  try {
+    await db.query("INSERT INTO blogs (title) VALUES ($1)", [blog]);
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-//Updating  blog entry
 app.post("/edit", async (req, res) => {
-    const blogsTitle = req.body.updatedblogsTitle;
-    const blogsid = req.body.updatedblogsId;
+  const blogs = req.body.updatedblogsTitle;
+  const id = req.body.updatedItemId;
 
-    try {
-        await db.query("UPDATE blogs SET title = ($1) WHERE id = $2", [item, id]);
-        res.redirect("/");
-    }   catch (err) {
-        console.log(err);
-    }
+  try {
+    await db.query("UPDATE blogs SET title = ($1) WHERE id = $2", [item, id]);
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-    
+app.post("/delete", async (req, res) => {
+  const id = req.body.deleteblogId;
+  try {
+    await db.query("DELETE FROM blogs WHERE id = $1", [id]);
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-
-
-
-
-//Console log
 app.listen(port, () => {
-    console.log(`Server is running on ${port}`);
-  });
-  
+  console.log(`Server running on port ${port}`);
+});
